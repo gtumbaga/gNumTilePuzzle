013d19a675a5b34f51ebee52a83840098c96c2a3
Gabriel Tumbaga <gabrieltumbaga@odachi.local>
2023-10-09 12:38:33 -0700

    added dynamic div draw, and cssing. can now numerically add board size and tile amount

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-let maxTile = 4;
+const existingTiles = 48;
+let maxTile = 7;
 let openCol = maxTile;
 let openRow = maxTile;
+const rootSelector = document.querySelector(':root');
 
+
+const init = () => {
+    handleDrawTileDivs();
+    handleSetGameSize(3);
+}
+
+const handleDrawTileDivs = () => {
+    const tileStyle = document.createElement('style');
+    tileStyle.type='text/css';
+
+    for (let tile = 1; tile < (existingTiles+1); tile++) {
+        const tilesHolder = document.getElementById("tilesHolder");
+        const newDiv = document.createElement("div");
+        newDiv.className = "tile";
+        newDiv.id = `tile-${tile}`;
+        newDiv.innerHTML = tile;
+        tilesHolder.appendChild(newDiv);
+
+        const calculatedPX = (tile -1) * 100;
+        tileStyle.appendChild(document.createTextNode(`.col-${tile} { left: ${calculatedPX}px } .row-${tile} {top: ${calculatedPX}px}`));
+    }
+    document.getElementsByTagName('head')[0].appendChild(tileStyle);
+}
 
 const handleRandomize = () => {
     const totalTileCount = (maxTile*maxTile) -1;
-    for (let i = 0; i < 3000; i++) {
+    for (let i = 0; i < (totalTileCount*200); i++) {
         const rng = getRandomIntInclusive(1, totalTileCount);
         document.getElementById(`tile-${rng}`).click();
     }
@@ -13,10 +38,11 @@ const handleRandomize = () => {
 }
 
 const handleReset = () => {
+    console.log(`handleReset, maxTile is ${maxTile}`);
     const totalTileCount = (maxTile*maxTile) -1;
     let counter = 1;
-    for (let row = 1; row < 5; row++) {
-        for (let col = 1; col < 5; col++) {
+    for (let row = 1; row < (maxTile+1); row++) {
+        for (let col = 1; col < (maxTile+1); col++) {
             console.log(`tile: ${counter} col: ${col}, row: ${row} `);
 
             const tileDiv = document.getElementById(`tile-${counter}`);
@@ -29,12 +55,35 @@ const handleReset = () => {
                 break;
             }
         }
-        if (counter > totalTileCount) {
-            break;
-        }
+        //if (counter > totalTileCount) {
+            //break;
+        //}
     }
     openCol = maxTile;
     openRow = maxTile;
+}
+
+const handleSetGameSize = (size) => {
+    console.log(size);
+    maxTile = parseInt(size);
+
+
+    const totalTileCount = (maxTile*maxTile);
+    console.log(`total tile count set to ${totalTileCount}`);
+    let counter = 1;
+    for (let tile = 1; tile < (existingTiles+1); tile++) {
+        const tileDiv = document.getElementById(`tile-${tile}`);
+        if (tile < totalTileCount) {
+            tileDiv.style.display = "block";
+        } else {
+            tileDiv.style.display = "none";
+        }
+    }
+
+    const tileSize = 100;
+    const calculatedBoardSize = (tileSize * maxTile);
+    rootSelector.style.setProperty('--boardsize',`${calculatedBoardSize}px`);
+    handleReset();
 }
 
 const handleClick = (e) => {
@@ -87,3 +136,7 @@ const getRandomIntInclusive = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
+
+
+
+init();
